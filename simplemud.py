be8e88414cc0b139c0282ee95b5c5bba309aa520
Conflict <conflict@localhost.localdomain>
2014-08-09 20:37:18 -0400

    Added comments and cleaned up code a little

--- simplemud.py
+++ simplemud.py
@@ -19,6 +19,7 @@ Some ideas for things to try adding:
 
 # import the MUD server class
 from mudserver import MudServer
+# import time for the "100% CPU hack"
 import time
 
 # structure defining the rooms in the game. Try adding more rooms to the game!
@@ -243,4 +244,6 @@ while True:
         else:
             # send back an 'unknown command' message
             mud.send_message(id, "Unknown command '%s'" % command)
+
+    # a hack to "fix" the 100% CPU issue        
     time.sleep(0.05)
